67d9c63f614661bf9bcfc00af855820dd10d25bc
IsacristineReis <isacrisbelle21@gmail.com>
2026-08-25 03:05:42 -0300

    Adição de uma ToDo list

---
+++ ProjectsBeginners/ToDo.py
@@ -0,0 +1,29 @@
+'''
+- Load existing items
+- Creat a new item
+- List items
+- Mark item as complete
+- Save items
+'''
+
+print("===============================")
+print("     List Management APP       ")
+print("===============================")
+
+def load_tasks():
+    pass
+
+def save_tasks():
+    pass
+
+def view_tasks():
+    pass
+
+def create_tasks():
+    pass
+
+def mark_task_complete():
+    pass
+
+def main():
+    pass
